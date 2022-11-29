956f70fb16f04c0038c78f45e35620052e475445
SamoriR <samori.roberts@gmail.com>
2022-11-28 17:43:40 -0800

    Photoreel and edit homescreen text

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 import { useLoaderData } from 'react-router-dom';
-import classnames from 'classnames';
 
 import Container from 'libs/components/Container';
 import SocialIcons from 'libs/components/social/icons';
@@ -8,12 +7,7 @@ import SocialLink from 'libs/components/social/SocialLink';
 import NavigationWrapper from 'libs/components/navigation';
 import { IArticle } from 'libs/api/getArticles';
 
-import image1 from 'res/photos/home/img-1.jpg';
-import image2 from 'res/photos/home/img-2.jpg';
-import image3 from 'res/photos/home/img-3.jpg';
-import image4 from 'res/photos/home/img-4.jpg';
-import image5 from 'res/photos/home/img-5.jpg';
-
+import PhotoReel from './PhotoReel';
 import ArticleCard from './ArticleCard';
 import NewsletterCard from './NewsletterCard';
 import ResumeCard from './ResumeCard';
@@ -24,33 +18,6 @@ const {
   GitHubIcon,
   LinkedInIcon,
 } = SocialIcons;
-
-function Photos() {
-  const rotations = ['rotate-2', '-rotate-2', 'rotate-2', 'rotate-2', '-rotate-2'];
-
-  return (
-    <div className="mt-16 sm:mt-20">
-      <div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
-        {[image1, image2, image3, image4, image5].map((image, imageIndex) => (
-          <div
-            key={image}
-            className={classnames(
-              'relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800 sm:w-72 sm:rounded-2xl',
-              rotations[imageIndex % rotations.length],
-            )}
-          >
-            <img
-              src={image}
-              alt=""
-              sizes="(min-width: 640px) 18rem, 11rem"
-              className="absolute inset-0 h-full w-full object-cover"
-            />
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-}
 
 export default function Home() {
   const articles = useLoaderData() as IArticle[];
@@ -70,6 +37,7 @@ export default function Home() {
             My name is Samori Roberts, graduate of Morehouse College, class of 2018,
             and Seattle based Software Engineer. I&apos;m a Microsoft Alumni that loves
             working with early-stage start-ups as a contractor, advisor, and leader.
+            (And yes I did take those pictures of Travis Scott and J-Cole below!)
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink
@@ -95,7 +63,9 @@ export default function Home() {
           </div>
         </div>
       </Container>
-      <Photos />
+
+      <PhotoReel />
+
       <Container outerClassName="mt-24 md:mt-28">
         <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
           <div className="flex flex-col gap-16">
